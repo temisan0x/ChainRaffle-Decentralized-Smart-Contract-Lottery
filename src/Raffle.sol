a2611f3390a7c7498ab44a27cdf00b3db559125f
temycodes <temycodes@gmail.com>
2023-11-30 10:48:18 +0100

    error - revert

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,13 +9,19 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
+    error Raffle__NotEnoughEthSent();
     uint256 private immutable i_entranceFee;
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public {}
+    function enterRaffle() external payable {
+       // require(msg.value >=  i_entranceFee, "Not enough ETH sent!");
+       if(msg.value < i_entranceFee){
+        revert Raffle__NotEnoughEthSent();
+       }
+    }
 
     function pickWinner() public {}
 
