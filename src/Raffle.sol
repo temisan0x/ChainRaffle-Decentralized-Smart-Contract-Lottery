385a32aec277a712a32715444215f6e0043a9ec8
temycodes <temycodes@gmail.com>
2023-12-29 09:45:36 +0100

    added commented order of layout

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,3 +1,53 @@
+// Order of Layout
+
+// Contract elements should be laid out in the following order:
+
+//     Pragma statements
+
+//     Import statements
+
+//     Events
+
+//     Errors
+
+//     Interfaces
+
+//     Libraries
+
+//     Contracts
+
+// Inside each contract, library or interface, use the following order:
+
+//     Type declarations
+
+//     State variables
+
+//     Events
+
+//     Errors
+
+//     Modifiers
+
+//     Functions
+
+
+// Order of Functions
+//     constructor
+
+//     receive function (if exists)
+
+//     fallback function (if exists)
+
+//     external
+
+//     public
+
+//     internal
+
+//     private
+
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
